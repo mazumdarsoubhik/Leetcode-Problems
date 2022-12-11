e7f2ad18a25ff54e1dc1db9d617609c9ee7bb743
Soubhik <44722027+mazumdarsoubhik@users.noreply.github.com>
2022-12-11 16:01:32 +0530

    Update Pattern_Problems.c

--- Pattern_Problems.c
+++ Pattern_Problems.c
@@ -33,3 +33,69 @@ int main()
     return 0;
 }
 
+/******************************************************************************
+
+                        Cat Ear with Real Number
+                        Time complexity: O(n)
+Output:
+1      1 
+12    21 
+123  321 
+12344321
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    printf("Cat Ear with Real Number\n");
+    int n = 4;    
+    char b[100];
+    for (int i=0; i < 2*n; i++) b[i] = ' ';
+    
+    for (int j=1; j <= n ; j++){
+        b[j-1] = j + '0';
+        b[2*n-j] = j + '0';
+        printf("%s %s", b, "\n");
+    }
+
+    return 0;
+}
+
+/******************************************************************************
+
+                        Right triangle with real numbers
+                        Time complexity: O(n)
+Output:
+1  
+2  3  
+4  5  6  
+7  8  9  10  
+11  12  13  14  15
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    printf("Right triangle with real numbers \n");
+    
+    int n = 15;
+    int o=0;
+    
+    for (int i=1; o<=n-1; i++){
+        for (int j=0;j<i && o <= n-1;j++) {
+            o++;
+            printf("%d %c", o,' ');
+        }
+        printf("%s","\n");
+    }
+
+    return 0;
+}
